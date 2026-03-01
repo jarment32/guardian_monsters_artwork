f272dddbdfaa9fd88edeaae44fe6f72d7027d79a
jarment32 <130186056+jarment32@users.noreply.github.com>
2026-03-01 17:01:06 +0100

    Actualizar game.js

--- game.js
+++ game.js
@@ -1,8 +1,5 @@
-export let inBattle = false;
-
 export function startGame() {
     document.getElementById('enterArena').style.display = 'none';
-
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
 
@@ -13,7 +10,7 @@ export function startGame() {
     window.addEventListener('resize', resize);
     resize();
 
-    // TODO: aquí va toda la lógica de tu juego usando los assets en sprites/, tilesets/, backdrops/
+    // Aquí irá toda la lógica de tu juego usando sprites/, tilesets/, backdrops/, ui/
     ctx.fillStyle = 'white';
-    ctx.fillRect(50,50,100,100); // ejemplo temporal
+    ctx.fillRect(50,50,100,100); // placeholder
 }
