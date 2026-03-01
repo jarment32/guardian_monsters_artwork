88b4070f512eeeb736852c97fddd3ea7a0be323e
jarment32 <130186056+jarment32@users.noreply.github.com>
2026-03-01 17:22:52 +0100

    Crear gameAssets.js

---
+++ gameAssets.js
@@ -0,0 +1,9 @@
+export const worldAssets = {
+  background: new Image(),
+  building_shop: new Image(),
+  building_arena: new Image()
+};
+
+worldAssets.background.src = 'tilesets/limbusdev_world2.png';
+worldAssets.building_shop.src = 'sprites/ui/shop.png'; 
+worldAssets.building_arena.src = 'sprites/ui/arena.png';
